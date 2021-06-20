fba0063f5fa41b3e381e0e2838b7981024bca913
Oscar Ablinger <dev.ablinger@gmail.com>
2021-06-20 18:11:10 +0200

    feat: implement basic website

---
+++ index.js
@@ -0,0 +1,39 @@
+(function() {
+    window.addEventListener('load', function() {
+        let aLinkIsFocused = false
+        let hoveringColor = undefined
+
+        const links = document.querySelector('.links')
+        const wrapper = document.querySelector('.wrapper')
+
+        ;['github', 'medium', 'linkedin', 'twitter'].forEach(link => {
+            const el = links.getElementsByClassName(link)[0].getElementsByTagName('a')[0]
+
+            el.addEventListener('mouseenter', function() {
+                hoveringColor = link
+                if (!aLinkIsFocused) {
+                    wrapper.dataset.selectedLink = link
+                }
+            })
+            el.addEventListener('mouseleave', function() {
+                hoveringColor = undefined
+                if (!aLinkIsFocused) {
+                    delete wrapper.dataset.selectedLink
+                }
+            })
+
+            el.addEventListener('focusin', function() {
+                aLinkIsFocused = true
+                wrapper.dataset.selectedLink = link
+            })
+            el.addEventListener('focusout', function() {
+                aLinkIsFocused = false
+                if (hoveringColor) {
+                    wrapper.dataset.selectedLink = hoveringColor
+                } else {
+                    delete wrapper.dataset.selectedLink
+                }
+            })
+        });
+    })
+})()
